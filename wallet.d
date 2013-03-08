6a49e2a7957f2cd7f42573c00ab0d37daa8327f0
RawIron <michael_nathe@hotmail.com>
2013-03-07 19:03:19 -0800

    write threaded wallet in D

---
+++ wallet.d
@@ -0,0 +1,42 @@
+#!/usr/bin/env rdmd
+
+import std.concurrency, std.stdio;
+
+class Wallet {}
+
+
+interface ControlEvent {
+    bool isStopped();
+    void shutdown();
+}
+
+interface WalletEvent {
+    void execute();
+}
+class WalletWithdraw : WalletEvent {
+    void execute() {
+    }
+}
+
+
+void worker() {
+    while (true) {
+        auto event = receiveOnly!(Tid, WalletEvent)();
+        writeln("done");
+        event[0].send(thisTid);
+    }
+}
+
+
+void main() {
+    WalletEvent[] jobs = [new WalletWithdraw(), new WalletWithdraw(), ];
+    immutable WalletEvent job;
+
+    auto tid = spawn(&worker);
+    foreach (job; jobs) {
+        writeln("send");
+        tid.send(thisTid, job);
+        receiveOnly!Tid();
+    }
+}
+
