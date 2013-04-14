3bbd9c57ac368d0067793ba05895ed289019471c
RawIron <manianology@gmail.com>
2013-04-13 22:38:04 -0700

    playing ..'

--- basics.d
+++ basics.d
@@ -1,14 +1,20 @@
 #!/usr/bin/env rdmd
 
-import std.stdio;
+import std.stdio, std.string, std.algorithm;
 
-void doSomething(immutable int times) {
-    immutable message = "hello";
-    foreach (iter; 0..times) {
-        writeln(message);
+void doSomething(in string line) {
+    void write(string aWord) {
+        writeln(aWord);
+    }
+
+    ulong[string] dict;
+    foreach (word; splitter(strip(line))) {
+        write(word);
+        dict[word] = dict.length;
     }
 }
 
 void main() {
-    doSomething(5);
+    immutable message = "this is text for a test";
+    doSomething(message);
 }
