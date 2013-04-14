740c151980b269ea40b9881de711dd0b094d97b7
RawIron <manianology@gmail.com>
2013-04-13 22:56:51 -0700

    playing unittest

--- basics.d
+++ basics.d
@@ -2,6 +2,19 @@
 
 import std.stdio, std.string, std.algorithm;
 
+
+unittest {
+    int topic() {
+        return 25;
+    }
+
+    void test_returnsSum25() {
+        assert(topic() == 25);
+    }
+
+    test_returnsSum25();
+}
+
 void doSomething(in string line) {
     void write(string aWord) {
         writeln(aWord);
