5b1bf709dca776d9606e7370c1a3dfdd79fbb90c
RawIron <manianology@gmail.com>
2013-05-03 08:14:25 -0700

    d file io, string

--- fileReader.d
+++ fileReader.d
@@ -5,15 +5,27 @@ import std.file;
 import std.string;
 
 
-void main() {
+string fileReaderUsingReadText() {
     string fileName = "fileReader.d";
     string content = readText(fileName);
     writeln(content);
+    return content;
+}
 
+int iterateBySplitLines(in string content) {
     string[] lines = splitLines(content);
+    int countI = 0;
     foreach (string line; lines) {
         if (line.startsWith("i")) {
-            writeln("y");
+            ++countI;
         }
     }
+    return countI;
 }
+
+
+void main() {
+    auto content = fileReaderUsingReadText();
+    iterateBySplitLines(content);
+}
+
