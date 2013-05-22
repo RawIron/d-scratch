ef720428a25386272f045ed5c39ee7be4aefe96e
RawIron <manianology@gmail.com>
2013-05-22 08:00:53 -0700

    d streams

--- streamProcessing.d
+++ streamProcessing.d
@@ -4,14 +4,26 @@ import std.algorithm : chain, map, copy;
 import std.stdio;
 
 
+void printGreetingsWithLambda() {
+    string[] names = ["Anders", "David", "James", "Jeff", "Joe", "Erik"];
+    auto f = function string(string a) { return ("Hello! " ~ a); };
+    foreach (name; names) {
+        writeln(f(name));
+    }
+}
+
 void streamProcessing() {
-    ["Anders", "David", "James", "Jeff", "Joe", "Erik"]
-        .map!(str => chain("Hello! ", str, "\n"))
-        .copy(stdout.lockingTextWriter());
+    string[] names = ["Anders", "David", "James", "Jeff", "Joe", "Erik"];
+    auto greetings = map!(function string(string a) { return ("Hello!" ~ a ~ "\n"); })(chain(names));
+    foreach (greeting; greetings) {
+        write(greeting);
+    }
+    //copy(stdout.lockingTextWriter));
 }
 
 
 void main() {
+    printGreetingsWithLambda();
     streamProcessing();
 }
 
