3b235a4bedbf559abf204b2ea5377ff1fc4ad67c
RawIron <manianology@gmail.com>
2013-05-23 23:04:55 -0700

    d phobos

--- streamProcessing.d
+++ streamProcessing.d
@@ -1,7 +1,7 @@
 #!/usr/bin/env rdmd
 
-import std.algorithm : chain, map, copy;
-import std.stdio;
+import std.algorithm : chain, map, copy, Result;
+import std.stdio : write, writeln;
 
 
 void printGreetingsWithLambda() {
@@ -12,18 +12,20 @@ void printGreetingsWithLambda() {
     }
 }
 
-void streamProcessing() {
-    string[] names = ["Anders", "David", "James", "Jeff", "Joe", "Erik"];
-    auto greetings = map!(function string(string a) { return ("Hello!" ~ a ~ "\n"); })(chain(names));
-    foreach (greeting; greetings) {
-        write(greeting);
-    }
-    //copy(stdout.lockingTextWriter));
+Result streamProcessing(string[] names) {
+    return map!(function string(string a) { return ("Hello!" ~ a ~ "\n"); })(names);
 }
 
 
 void main() {
+    string[] names = ["Anders", "David", "James", "Jeff", "Joe", "Erik"];
+    Result greetings;
+
     printGreetingsWithLambda();
-    streamProcessing();
+    greetings = streamProcessing(names);
+    foreach (greeting; greetings) {
+        write(greeting);
+    }
+    //copy(stdout.lockingTextWriter));
 }
 
