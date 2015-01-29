d0a7980fa90d7312a534430baf2824fc982ccc06
RawIron <manianology@gmail.com>
2015-01-29 08:37:21 -0800

    templates, bootstrap started

--- hackerrank/ArraysAndSorting/insertion_sort.d
+++ hackerrank/ArraysAndSorting/insertion_sort.d
@@ -1,14 +1,38 @@
 #!/usr/bin/rdmd
 
-import std.stdio;
+import std.stdio, std.conv;
 
 
-enum ENV = {LOCAL, SUBMIT};
-enum WHAT = {TOTAL_TESTS, TOTAL_ENTRIES, ENTRIES};
+enum ENV: int {LOCAL, SUBMIT};
+enum WHAT: int {TOTAL_TESTS, TOTAL_ENTRIES, ENTRIES};
 
 struct conf {
   immutable ENV env;
   WHAT read_what;
+}
+
+template match(T...){
+  string data = din.readLine();
+}
+
+template match(ENV e : SUBMIT) {
+  string data = din.readLine();
+}
+
+template match(ENV e : LOCAL, WHAT w: TOTAL_TESTS) {
+  string data = "1";
+}
+
+template match(ENV e : LOCAL, WHAT w: TOTAL_ENTRIES) {
+  string data = "3";
+}
+
+template match(ENV e : LOCAL, WHAT w: ENTRIES) {
+  string data = "3 5 8";
+}
+
+string reader(conf) {
+  return match!(conf.env, conf.read_what).data;
 }
 
 
@@ -24,29 +48,17 @@ void print_sorted(int[] sorted) {
   }
 }
 
-void reader(conf) {
-  match(conf.env) {
-  SUBMIT: din.readLine()
-  LOCAL:
-    match(conf.read_what) {
-      TOTAL_TESTS: "1"
-      TOTAL_ENTRIES: "3"
-      ENTRIES: "3 5 8"
-    }
-  }
-}
-
 
 int main() {
-  conf conf = {LOCAL, TOTAL_TESTS}
+  conf conf = {LOCAL, TOTAL_TESTS};
 
   immutable int numberOfEntries;
   immutable int numberOfTests;
   int[] population = new int[numberOfEntries];
 
-  numberOfEntries = std.conv.toInt(reader())
+  numberOfEntries = toInt(reader(conf));
 
-  for (0 .. numberOfTests) {
+  foreach (i; 0 .. numberOfTests) {
     population = splitter(strip("3 5 1"));
 
     insertion_sort(population);
