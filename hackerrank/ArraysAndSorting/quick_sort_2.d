7b070ee6f26e92be8d9235785ce440f30086ad10
RawIron <manianology@gmail.com>
2015-02-04 08:19:07 -0800

    add print of sub-arrays

--- hackerrank/ArraysAndSorting/quick_sort_2.d
+++ hackerrank/ArraysAndSorting/quick_sort_2.d
@@ -13,6 +13,7 @@ void print(int[] sorted)
   writeln;
 }
 
+
 int[] swapIfReverse(int[] that, int left, int right)
 {
   if (that[left] < that[right]) return that;
@@ -43,7 +44,12 @@ int[] quicksort(int[] that)
       right ~= entry;
   }
 
-  return quicksort(left) ~ p ~ quicksort(right);
+  int[] leftSorted = quicksort(left);
+  if (leftSorted.length > 1) print(leftSorted);
+  int[] rightSorted = quicksort(right);
+  if (rightSorted.length > 1) print(rightSorted);
+
+  return leftSorted ~ p ~ rightSorted;
 }
 
 
@@ -55,7 +61,7 @@ int main()
   //numberOfEntries = to!int(din.readLine());
   //population = splitter(din.readLine().strip(' ')).map!(to!int).array;
   numberOfEntries = 7;
-  population = splitter("3 5 2 8 1 12 7".strip(' ')).map!(to!int).array;
+  population = splitter("5 8 1 3 7 9 2".strip(' ')).map!(to!int).array;
 
   int[] sorted = quicksort(population);
 
