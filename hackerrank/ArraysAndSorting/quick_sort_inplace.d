3df8c662b8d31badac5a65b62cb223c1adf46198
RawIron <manianology@gmail.com>
2015-02-07 12:36:00 -0800

    pprint

--- hackerrank/ArraysAndSorting/quick_sort_inplace.d
+++ hackerrank/ArraysAndSorting/quick_sort_inplace.d
@@ -8,6 +8,15 @@ import std.array;
 int[] emptyArray;
 
 
+enum ptree = true;
+enum tabSize = 2;
+
+struct Print {
+  int height = 0;
+  char[tabSize] tab = "  ";
+  char[] ident;
+}
+
 void print(int[] sorted)
 {
   foreach (entry; sorted) {
@@ -16,12 +25,6 @@ void print(int[] sorted)
   writeln;
 }
 
-
-int[] swapIfReverse(int[] that, int i, int j)
-{
-  if (that[i] < that[j]) return that;
-  else return swap(that, i, j);
-}
 
 int[] swap(int[] that, int i, int j)
 {
@@ -34,9 +37,13 @@ int[] swap(int[] that, int i, int j)
 
 int[] quicksort(int[] that)
 {
-  int[] inplace(int[] that, int left, int p, int right)
+  int[] inplace(int[] that, int left, int p, int right, Print d)
   {
-    if (left >= right) return emptyArray;
+    ++d.height;
+    d.ident ~= d.tab;
+    writefln("%s called %d %d %d", d.ident, left, p, right);
+
+    if (left > right) return emptyArray;
     
     int cutHere = 0;
     int i = 0;
@@ -59,21 +66,31 @@ int[] quicksort(int[] that)
       ++i;
     }
 
-    int[] sorted = swap(that, left + cutHere, right+1);
+    if (cutHere < that[left..right].length) {
+      int[] sorted = swap(that, left + cutHere, right+1);
+    }
+    else {
+    }
 
-    int[] leftSorted = inplace(that, left, that[left + cutHere], left + cutHere-1);
-    write("left");
-    if (leftSorted[].length > 1) print(leftSorted);
+    int[] leftSorted = inplace(that, left, that[left + cutHere], left + cutHere-1, d);
+    writef("%s left ", d.ident);
+    writefln("%s", leftSorted);
+    //if (leftSorted.length > 1) print(leftSorted);
 
-    int[] rightSorted = inplace(that, left + cutHere+1, that[right+1], right);
-    write("right");
-    if (rightSorted[].length > 1) print(rightSorted);
+    int[] rightSorted = inplace(that, left + cutHere+1, that[right+1], right, d);
+    writef("%s right ", d.ident);
+    writefln("%s", rightSorted);
+    //if (rightSorted.length > 1) print(rightSorted);
 
     return that[left..right+1];
   }
 
+  static if (ptree) {
+    Print d;
+  }
+
   if (that.length <= 1) return that;
-  else return inplace(that, 0, that[$-1], to!int(that.length-2));
+  else return inplace(that, 0, that[$-1], to!int(that.length-2), d);
 }
 
 
@@ -85,7 +102,8 @@ int main()
   //numberOfEntries = to!int(din.readLine());
   //population = splitter(din.readLine().strip(' ')).map!(to!int).array;
   numberOfEntries = 7;
-  population = splitter("5 8 1 3 7 9 2".strip(' ')).map!(to!int).array;
+  //population = splitter("5 8 1 3 7 9 2".strip(' ')).map!(to!int).array;
+  population = splitter("2 1 7".strip(' ')).map!(to!int).array;
 
   int[] sorted = quicksort(population);
 
